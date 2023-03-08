4793cc7dd5416018b91e8ef0265fc858735211b9
menegass0 <victormenegasso@unisantos.br>
2023-03-08 09:59:43 -0300

    fim-do-script

--- uncovering-image-behind/script.js
+++ uncovering-image-behind/script.js
@@ -1,1 +1,19 @@
 document.querySelector('.teste');
+
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+ 
+setInterval(() => {
+    let random = null;
+    let randomInd = null;
+    if(numbersArray.length>0){
+        random = Math.floor(Math.random() * (numbersArray.length - 1))
+        randomInd = numbersArray[random];
+        let block = document.querySelector('#b'+randomInd);
+        block.style.opacity = 0;
+        numbersArray.splice(random, 1);
+    }
+    else{
+        return ;
+    }
+}, 500);
+
